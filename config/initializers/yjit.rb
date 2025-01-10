08196259cb724703e4869736e5a0a0fa946fbdab
Martin Gruner <mg@zammad.com>
2025-01-10 10:46:54 +0100

    Add an initializer that allows disabling of YJIT

---
+++ config/initializers/yjit.rb
@@ -0,0 +1,6 @@
+# Copyright (C) 2012-2025 Zammad Foundation, https://zammad-foundation.org/
+
+# Rails enables YJIT by default. Provide a way to disable it in case of issues.
+if %w[1 true].include?(ENV['ZAMMAD_DISABLE_YJIT'])
+  Rails.application.config.yjit = false
+end
