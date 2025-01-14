46d071dd8ee200e11bb2804a152c1ae3b3ee3952
Martin Gruner <mg@zammad.com>
2025-01-14 13:41:26 +0100

    Use --pull with docker build

--- spec/fixtures/files/smime/generate/run.sh
+++ spec/fixtures/files/smime/generate/run.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o pipefail
 
-docker build --no-cache --tag zammad/smime-test-certificates:latest .
+docker build --pull --no-cache --tag zammad/smime-test-certificates:latest .
 
 docker run --rm --env=SKIP_REGENERATE=${SKIP_REGENERATE:=""} --volume "$(pwd)/../:/etc/ssl/certs" zammad/smime-test-certificates:latest
